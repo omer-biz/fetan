62ac248314d5be5017ec9cf40330df0fb121422d
omer <omerabdi@protonmail.com>
2025-01-25 18:04:30 +0300

    scoreboard html

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::get('/', fn () => view('index'))->name('home');
 
 Route::get('/faq', fn() => view('faq'))->name('faq');
 
+Route::get('/scoreboard', fn() => view('scoreboard'))->name('scoreboard');
+
 Route::get('/contact-us', fn() => view('contact-us'))->name('contact-us');
 Route::post('/contact-us', [ContactUs::class, 'submitContactForm'])->name('contact-us.post');
