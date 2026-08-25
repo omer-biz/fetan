0dc830ba70be1770d49efb8d8d8c9fb210a7ac42
omer-biz <omerabdi@protonmail.com>
2026-08-25 10:55:58 +0300

    style: switch theme toggle to svg icons and match vesl.et background exactly

--- src/Main.elm
+++ src/Main.elm
@@ -6,6 +6,8 @@ import Browser.Events exposing (onKeyDown, onKeyUp)
 import Dict exposing (Dict, update)
 import Dictation as DictGen
 import Html exposing (Html, a, div, main_, option, p, select, span, table, tbody, td, text, tr)
+import Svg exposing (svg, path)
+import Svg.Attributes as SvgAttr
 import Html.Attributes exposing (class, href, selected, tabindex, target, value)
 import Html.Events exposing (onBlur, onFocus, onInput, preventDefaultOn)
 import Json.Decode as Decode
@@ -519,20 +521,41 @@ updateScore metrics =
     { metrics | score = score }
 
 
+sunIcon : Html msg
+sunIcon =
+    svg
+        [ SvgAttr.viewBox "0 0 24 24", SvgAttr.fill "currentColor", class "w-6 h-6" ]
+        [ path
+            [ SvgAttr.d "M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z"
+            ]
+            []
+        ]
+
+moonIcon : Html msg
+moonIcon =
+    svg
+        [ SvgAttr.viewBox "0 0 24 24", SvgAttr.fill "currentColor", class "w-6 h-6" ]
+        [ path
+            [ SvgAttr.fillRule "evenodd", SvgAttr.clipRule "evenodd"
+            , SvgAttr.d "M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+            ]
+            []
+        ]
+
 viewThemeToggle : Theme -> Html Msg
 viewThemeToggle theme =
     let
         icon =
             if theme == Dark then
-                "☀️"
+                sunIcon
             else
-                "🌙"
+                moonIcon
     in
     Html.button
         [ Html.Events.onClick ToggleTheme
-        , class "absolute top-6 right-6 text-2xl opacity-70 hover:opacity-100 transition-opacity"
+        , class "absolute top-6 right-6 text-stone-600 dark:text-stone-400 opacity-70 hover:opacity-100 transition-opacity"
         ]
-        [ text icon ]
+        [ icon ]
 
 view : Model -> Html Msg
 view model =
