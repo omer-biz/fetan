eb102c5ab9db2e22f16354f63a86cb9b093ddfdf
omer-biz <omerabdi@protonmail.com>
2026-09-06 21:06:10 +0300

    Revert "feat: add blinking caret to the active dictation letter"
    This reverts commit 4e081bab978014928c92d77e25baa61650b0fd2a.

--- src/View.elm
+++ src/View.elm
@@ -426,13 +426,6 @@ viewDictation isFocused dict =
                     else
                         "text-stone-800 dark:text-stone-200"
 
-                caret =
-                    if isCurrent then
-                        span [ class "absolute left-0 top-[0.1em] bottom-[0.1em] w-[2px] bg-slate-500 dark:bg-slate-400 animate-blink rounded-full" ] []
-
-                    else
-                        text ""
-
                 classes =
                     String.join " " [ "relative rounded-sm py-0.5", spaceClass, colorClass ]
             in
@@ -445,8 +438,7 @@ viewDictation isFocused dict =
                   else
                     class ""
                 ]
-                [ caret
-                , if isSpace then
+                [ if isSpace then
                     text " "
 
                   else
